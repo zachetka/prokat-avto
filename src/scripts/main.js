6d7e83d18c1e79a6fcf3a05fbab2f43f7f831fca
Zachetka <axi83@mail.ru>
2021-02-10 12:03:35 +0300

    finish sections slider and advantages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,31 @@
 $(function(){
-    // $('.reviews__slider').slick({
-    //     arrows: false,
-    //     dots: true,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     responsive: [
-    //       {
-    //         breakpoint: 1100,
-    //         settings: {
-    //           slidesToShow: 3
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 800,
-    //         settings: {
-    //           slidesToShow: 2
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 600,
-    //         settings: {
-    //           slidesToShow: 1
-    //         }
-    //       }
-    //     ]        
-    // });
+    $('.slider__inner').slick({        
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: '<button class="slick-arrow slick-arrow--prev"><img src="assets/images/icons/left-arrow.svg" alt="icon: arrow-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next"><img src="assets/images/icons/right-arrow.svg" alt="icon: arrow-right"></button>',
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]        
+    });
 
     $('.menu__btn').on('click', function(){
         $('.menu__list').toggleClass('menu__list--active');
