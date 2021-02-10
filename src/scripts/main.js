9548a16a0a3127e9eabd17922241948d0111b89f
Zachetka <axi83@mail.ru>
2021-02-10 10:33:32 +0300

    finish sections header and cover

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    // $('.reviews__slider').slick({
+    //     arrows: false,
+    //     dots: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     responsive: [
+    //       {
+    //         breakpoint: 1100,
+    //         settings: {
+    //           slidesToShow: 3
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 800,
+    //         settings: {
+    //           slidesToShow: 2
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 600,
+    //         settings: {
+    //           slidesToShow: 1
+    //         }
+    //       }
+    //     ]        
+    // });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+});
